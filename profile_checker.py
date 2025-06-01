53a03f89e3f580096e798b97485aa0649cddf140
AlaaHarmoush1 <Alaacoding@outlook.com>
2025-06-01 18:07:30 +0300

    updated the code to only accept GPA between 0 & 5

--- profile_checker.py
+++ profile_checker.py
@@ -6,11 +6,15 @@ gpa = float(input("Enter your GPA:"))
 field_of_interest = input("Enter your field of interest:")
 graduated = input("Are you graduated? (yes or no): ").lower()
 
-
-if age <= 25 and gpa >= 3.5 and graduated == "yes":
-    print(f"You are eligible for a scholarship in {field_of_interest} field")
-elif age < 30 and gpa >= 2.5 and age > 25:
-    print(f"You are eligible for an internship ")
+if gpa > 5 or gpa < 0:
+    print("Invalid gpa, please enter a value between 0 and 5")
 else:
-    print("apply again later")
+    if age <= 25 and gpa >= 3.5 and graduated == "yes":
+        print(f"You are eligible for a scholarship in {field_of_interest} field")
+    elif age < 30 and gpa >= 2.5 and age > 25:
+        print(f"You are eligible for an internship ")
+    else:
+        print("apply again later")
+
+
 
