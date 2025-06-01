de1caa7b3e22b162f07ab14bc689a1a4bc0b26eb
AlaaHarmoush1 <Alaacoding@outlook.com>
2025-06-01 12:24:37 +0300

    added int before the age

--- profile_checker.py
+++ profile_checker.py
@@ -1,12 +1,16 @@
 print("Hello and Welcom")
 
 name= input("Enter your name:")
-age = input("Enter your age:")
+age = int(input("Enter your age:"))
 gpa = float(input("Enter your GPA:"))
 field_of_interest = input("Enter your field of interest:")
 graduated = input("Are you graduated? (yes or no): ").lower()
 
 
-if age < 25 and gpa >= 3.5 and graduated == "yes":
-    print(f"You are eligibl for a scholarship in {field_of_interest} field")
+if age <= 25 and gpa >= 3.5 and graduated == "yes":
+    print(f"You are eligible for a scholarship in {field_of_interest} field")
+elif age < 30 and gpa >= 2.5:
+    print(f"You are eligible for an internship ")
+else:
+    print("apply again later")
 
